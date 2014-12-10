050a2f4caf2a723a5ae93ffee0323fec7fb83719
Natã Rafael <nata.erafael@gmail.com>
2014-12-10 03:28:47 -0200

    Ajustes no visual da página home, deixar mais bonito

--- src/application/views/page/home.php
+++ src/application/views/page/home.php
@@ -1,6 +1,7 @@
 <section id="corpo">
 	<div class="container">
 		<div class="row">
+		
 			<div class="col-md-12">
 				<h2>Planos</h2>
 				<div class="row">
@@ -34,38 +35,125 @@
 			</div>
 			<div class="col-md-12">
 				<hr>
-				<h2>bonus</h2>
+				<h2>Bonus</h2>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				
 				<div class="row">
-					<div class="col-md-3">
-						<h3>Bonus 1</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dol</p>
-					</div>
+					<div class="col-md-12">
 
-					<div class="col-md-3">
-						<h3>Bonus 2</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dol</p>
-					</div>
+						<div id="the-carousel" class="carousel slide" data-ride="carousel">
+							<ol class="carousel-indicators">
+								<li data-target="#the-carousel" data-slide-to="0" class=""></li>
+								<li data-target="#the-carousel" data-slide-to="1" class="active"></li>
+								<li data-target="#the-carousel" data-slide-to="2" class=""></li>
+								<li data-target="#the-carousel" data-slide-to="3" class=""></li>
+							</ol>
 
-					<div class="col-md-3">
-						<h3>Bonus 3</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dol</p>
+						<div class="carousel-inner">
+							<div class="item active">
+								<div class="col-sm-6">
+									<img src="img/imagem-principal.png">
+								</div>
+								<div class="col-sm-6">
+										<h3>Bonus 1</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+										consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+										cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+										proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col-sm-6">
+									<img src="img/imagem-principal.png">
+								</div>
+								<div class="col-sm-6">
+										<h3>Bonus 2</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+										consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+										cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+										proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col-sm-6">
+									<img src="img/imagem-principal.png">
+								</div>
+								<div class="col-sm-6">
+										<h3>Bonus 3</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+										consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+										cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+										proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+								</div>
+							</div>
+							<div class="item">
+								<div class="col-sm-6">
+									<img src="img/imagem-principal.png">
+								</div>
+								<div class="col-sm-6">
+										<h3>Bonus 4</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+										consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+										cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+										proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+								</div>
+							</div>
+						</div>
+
+						<a class="left carousel-control" href="#the-carousel" role="button" data-slide="prev">
+							<span class="glyphicon glyphicon-chevron-left"></span>
+						</a>
+						<a class="right carousel-control" href="#the-carousel" role="button" data-slide="next">
+							<span class="glyphicon glyphicon-chevron-right"></span>
+						</a>
 					</div>
-					
-					<div class="col-md-3">
-						<h3>Bonus 4</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dol</p>
+				</div>
+			</div>
+
+			<div class="col-md-12">
+				<hr>
+				<h2>Depoimentos</h2>
+				<div class="row persona">
+					<div class="col-md-2 col-md-offset-1">
+						<img src="img/persona1.jpg" alt="..." class="img-circle" width="150px">
+					</div>
+					<div class="col-md-8">
+						<h3><span class="glyphicon glyphicon-comment"></span> Beakman <small>descrição breve</small> </h3>
+						<p>Texto comentário do individuo ao lado que usou o QuímicoApp e gostou.Texto comentá do individua do lado que usou o QuímicoApp e gostou.Texto comentá do individua do lado que usou o QuímicoApp e gostou.</p>
+					</div>
+				</div>
+
+				<div class="row persona"> 
+					<div class="col-md-2 col-md-offset-1"> 
+						<img src="img/persona2.jpg" alt="..." class="img-circle" width="150px"> 
+					</div> 
+					<div class="col-md-8"> 
+						<h3><span class="glyphicon glyphicon-comment"></span> Albert Einstein <small>descrição breve</small> </h3> 
+						<p>Texto comentário do individuo ao lado que usou o QuímicoApp e gostou.Texto comentá do individua do lado que usou o QuímicoApp e gostou.Texto comentá do individua do lado que usou o QuímicoApp e gostou.</p>
+					</div>
+				</div>
+
+				<div class="row persona"> 
+					<div class="col-md-2 col-md-offset-1"> 
+						<img src="img/persona3.jpg" alt="..." class="img-circle" width="150px"> 
+					</div> 
+					<div class="col-md-8"> 
+						<h3><span class="glyphicon glyphicon-comment"></span> Carl sagan <small>descrição breve</small> </h3> 
+						<p>Texto comentário do individuo ao lado que usou o QuímicoApp e gostou.Texto comentá do individua do lado que usou o QuímicoApp e gostou.Texto comentá do individua do lado que usou o QuímicoApp e gostou.</p>
 					</div>
 				</div>
 			</div>
