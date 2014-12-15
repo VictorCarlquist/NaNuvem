f4084b0dcd99a5486d03bdfb3dde998b77bf192e
SusaneGoulart <susi-chaves@hotmail.com>
2014-12-15 21:46:28 -0200

    criacao dos planos e dos bonus e depoimentos

--- src/application/views/page/home.php
+++ src/application/views/page/home.php
@@ -4,6 +4,11 @@
 		
 			<div class="col-md-12">
 				<h2>Planos</h2>
+				
+				<p>O repositório de arquivos NaNuvem traz segurança para seu trabalho. Ele conta com três excelentes 
+				tipos de planos: o Plano Stratus, Plano Autocumulus e Plano Cirus. Conheça e escolha aqui o melhor 
+				plano que se adeque as suas necessidades sejam elas pessoais, comerciais e empresariais.</p>
+
 				<div class="row">
 
 					<?php foreach ($planos as $plano): ?>
@@ -18,14 +23,12 @@
 			</div>
 			<div class="col-md-12">
 				<hr>
-				<h2>Bonus</h2>
+				<h2>Bônus</h2>
 
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>O repositório de arquivos NaNuvem traz para nossos usuários cadastrados bônus que possuem o
+				objetivo de aprimorar cada vez mais o plano adquirido, tornando-o sempre melhor. Os bônus são 
+				conquistados de acordo com os desafios executados por eles. Venha fazer parte do NaNuvem e 
+				aprimore o seu plano!</p>
 				
 				<div class="row">
 					<div class="col-md-12">
@@ -116,7 +119,7 @@
 					</div>
 					<div class="col-md-8">
 						<h3><span class="glyphicon glyphicon-comment"></span> Beakman <small>descrição breve</small> </h3>
-						<p>Texto comentário do individuo ao lado que usou o QuímicoApp e gostou.Texto comentá do individua do lado que usou o QuímicoApp e gostou.Texto comentá do individua do lado que usou o QuímicoApp e gostou.</p>
+						<p>Já utilizei muito repositórios de arquivos, mas o NaNuvem me surpreendeu com sua interface atrativa e sua diversidade de recursos. Estou adorando utilizá-lo.</p>
 					</div>
 				</div>
 
@@ -126,7 +129,7 @@
 					</div> 
 					<div class="col-md-8"> 
 						<h3><span class="glyphicon glyphicon-comment"></span> Albert Einstein <small>descrição breve</small> </h3> 
-						<p>Texto comentário do individuo ao lado que usou o QuímicoApp e gostou.Texto comentá do individua do lado que usou o QuímicoApp e gostou.Texto comentá do individua do lado que usou o QuímicoApp e gostou.</p>
+						<p>O NaNuvem é um dos melhores repositórios de arquivos que já utilizei em minha vida, oferece uma excelente segurança aos meus arquivos, além de ter um acesso fácil e um uso satisfatório. Eu utilizo o NaNuvem em meus trabalhos e recomendo sempre aos meus amigos. Vale a pena!</p>
 					</div>
 				</div>
 
@@ -136,7 +139,7 @@
 					</div> 
 					<div class="col-md-8"> 
 						<h3><span class="glyphicon glyphicon-comment"></span> Carl sagan <small>descrição breve</small> </h3> 
-						<p>Texto comentário do individuo ao lado que usou o QuímicoApp e gostou.Texto comentá do individua do lado que usou o QuímicoApp e gostou.Texto comentá do individua do lado que usou o QuímicoApp e gostou.</p>
+						<p>O repositório de arquivos NaNuvem é excelente, desde quando o conheci até o prezado momento nunca me deixou na mão. Tem uma interface agradável, uma gama de recursos muitos úteis para o meu uso, além de oferecer diferentes tipos de planos a diferentes tipos de pessoas. Recomendo!</p>
 					</div>
 				</div>
 			</div>
